b8726fd020ba5ea7b02c521ebd84ab3199fa1f9f
siryu-saito <saito.s.ryu@gmail.com>
2021-05-25 17:28:36 +0900

    [Finish]配列 演習問題

---
+++ js_basic/js/exercise_array.js
@@ -0,0 +1,11 @@
+var scores = [100, 90, 80, 70, 60];
+var sum = 0;
+
+for (var i = 0; i < scores.length; i++) {
+  sum += scores[i];
+}
+
+var average = sum / scores.length;
+
+console.log('合計値:' + sum);
+console.log('平均値:' + average);
