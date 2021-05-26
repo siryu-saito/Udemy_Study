b10ddccdf21ca469e067924130950a1a59036eb9
siryu-saito <saito.s.ryu@gmail.com>
2021-05-26 22:56:04 +0900

    [Finish]イベントとイベントハンドラ 演習問題

---
+++ js_event/js/exercise.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+  var e = document.getElementById('button');
+  e.addEventListener('click', function(){
+    appendElement();
+  }, false);
+
+
+}, false);
+
+function appendElement() {
+  var result = document.getElementById('result');
+  var li = document.createElement('li');
+  var text = document.createTextNode(result.value);
+  li.appendChild(text);
+  lists.appendChild(li);
+}
